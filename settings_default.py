1bec89bd7a9ef71196b9b91bd4c5ff66153b82b5
Philipp Metzler <phil@goli.at>
2021-03-06 20:05:18 +0100

    Add default setting

---
+++ settings_default.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+EMAIL_FROM = "me@somewhere.com"
+EMAIL_TO = "me@somewhere.com"
+
+SMTP_SERVER = "sendmail.com"
+SMTP_USER = "me@somewhere.com"
+SMTP_PASSWORD = "123456789"
+
+KEYWORDS = [
+    "Computer",
+]
